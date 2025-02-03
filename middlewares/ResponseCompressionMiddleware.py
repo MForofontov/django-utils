d6921e63e9dafa95edb43a6851bb5cd62a4d5712
MForofontov <mykyta.forofontov@hotmail.com>
2025-02-03 22:53:40 +0000

    feat: refactor ResponseCompressionMiddleware to use __call__ method and improve response handling

--- middlewares/ResponseCompressionMiddleware.py
+++ middlewares/ResponseCompressionMiddleware.py
@@ -1,15 +1,45 @@
 import gzip
-from django.utils.deprecation import MiddlewareMixin
+from io import BytesIO
 from django.http import HttpResponse
 
-class ResponseCompressionMiddleware(MiddlewareMixin):
+class ResponseCompressionMiddleware:
     """
     Middleware to compress HTTP responses using gzip.
     
     This middleware compresses the content of HTTP responses with 'text/html' content type
     using gzip to reduce the size of the response.
     """
-    
+    def __init__(self, get_response):
+        """
+        Initialize the middleware with the given get_response callable.
+        
+        Parameters
+        ----------
+        get_response : callable
+            The next middleware or view in the chain.
+        """
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """
+        Handle the incoming request and get the response.
+        
+        Parameters
+        ----------
+        request : HttpRequest
+            The incoming HTTP request.
+        
+        Returns
+        -------
+        HttpResponse
+            The HTTP response from the next middleware or view.
+        """
+        # Get the response from the next middleware or view
+        response = self.get_response(request)
+        
+        # Process the response
+        return self.process_response(request, response)
+
     def process_response(self, request, response):
         """
         Compress the response content if the content type is 'text/html'.
@@ -27,9 +57,12 @@ class ResponseCompressionMiddleware(MiddlewareMixin):
             The processed HTTP response with compressed content if applicable.
         """
         # Check if the response content type is 'text/html'
-        if response['Content-Type'] == 'text/html':
+        if response.get('Content-Type') == 'text/html' and 'gzip' not in response.get('Content-Encoding', ''):
             # Compress the response content using gzip
-            response.content = gzip.compress(response.content)
+            buffer = BytesIO()
+            with gzip.GzipFile(fileobj=buffer, mode='wb') as gzip_file:
+                gzip_file.write(response.content)
+            response.content = buffer.getvalue()
             # Set the 'Content-Encoding' header to 'gzip'
             response['Content-Encoding'] = 'gzip'
             # Update the 'Content-Length' header with the length of the compressed content
